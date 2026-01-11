30de695ffc6235c13e5187984a7966e9fbaeed2b
SonHua <hualocson@gmail.com>
2026-01-11 16:52:57 +0700

    Update icon paths in manifest.ts to use absolute URLs for improved resource loading consistency.

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,25 +11,25 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#222326",
     icons: [
       {
-        src: "manifest-icon-192.maskable.png",
+        src: "/manifest-icon-192.maskable.png",
         sizes: "192x192",
         type: "image/png",
         purpose: "any",
       },
       {
-        src: "manifest-icon-192.maskable.png",
+        src: "/manifest-icon-192.maskable.png",
         sizes: "192x192",
         type: "image/png",
         purpose: "maskable",
       },
       {
-        src: "manifest-icon-512.maskable.png",
+        src: "/manifest-icon-512.maskable.png",
         sizes: "512x512",
         type: "image/png",
         purpose: "any",
       },
       {
-        src: "manifest-icon-512.maskable.png",
+        src: "/manifest-icon-512.maskable.png",
         sizes: "512x512",
         type: "image/png",
         purpose: "maskable",
