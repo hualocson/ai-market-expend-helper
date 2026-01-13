904a5ac2dfbd118e84147017b6c7d0cf9393b396
SonHua <hualocson@gmail.com>
2026-01-13 21:37:31 +0700

    Integrate @serwist/next for service worker configuration in next.config.ts, adding offline precaching and custom service worker paths.

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,25 @@
 import type { NextConfig } from "next";
 
+import withSerwistInit from "@serwist/next";
+import { spawnSync } from "node:child_process";
+
+// Using `git rev-parse HEAD` might not the most efficient
+// way of determining a revision. You may prefer to use
+// the hashes of every extra file you precache.
+const revision =
+  spawnSync("git", ["rev-parse", "HEAD"], { encoding: "utf-8" }).stdout ??
+  crypto.randomUUID();
+
+const withSerwist = withSerwistInit({
+  additionalPrecacheEntries: [{ url: "/~offline", revision }],
+  swSrc: "src/app/sw.ts",
+  swDest: "public/sw.js",
+});
+
 const nextConfig: NextConfig = {
   transpilePackages: ["lucide-react"],
   output: "standalone",
   htmlLimitedBots: /.*/,
 };
 
-export default nextConfig;
+export default withSerwist(nextConfig);
