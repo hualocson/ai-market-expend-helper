95c79688447119ffc8ddfd906fe287115088b366
SonHua <hualocson@gmail.com>
2026-01-13 21:51:20 +0700

    Remove unnecessary TypeScript reference directives from sw.ts to streamline service worker implementation.

--- src/app/sw.ts
+++ src/app/sw.ts
@@ -1,6 +1,3 @@
-/// <reference no-default-lib="true" />
-/// <reference lib="esnext" />
-/// <reference lib="webworker" />
 import { defaultCache } from "@serwist/next/worker";
 import type { PrecacheEntry, SerwistGlobalConfig } from "serwist";
 import { Serwist } from "serwist";
